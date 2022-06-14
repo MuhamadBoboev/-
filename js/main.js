eedd3db1bbd0c315a06b0f0d242ecdcda0cb0ba6
Muhammad <bmuhamad915@gmail.com>
2022-06-14 03:11:34 +0300

    a

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 'use script'
+const mainBlock = document.querySelector('.main');
+const overBlock = document.querySelector('.over');
+const fertigButton = document.querySelector('.fertig__button');
+
 const numberQuestion = document.querySelectorAll('.number-1');0
 const numberAnswer = document.querySelectorAll('.number');
 const gefunden = document.querySelectorAll('.gefunden__text');
@@ -34,7 +38,7 @@ const arrayAnswer = {
 }
 
 button.addEventListener('click', function() {
-  if ( numberQuestion[0].value === numberAnswer[0].value) {
+  if ( (numberQuestion[0].value === numberAnswer[0].value) && Number(numberQuestion[0].value) > 0 ) {
     document.querySelectorAll('.win-1')[0].style.backgroundColor='green';
     document.querySelectorAll('.win-1')[1].style.backgroundColor='green';
     gefunden[0].innerText =  numberQuestion[0].value;
@@ -54,5 +58,28 @@ button.addEventListener('click', function() {
   if ( arrayQuestion.number4 == arrayAnswer.number4) {
     console.log('4')
   }
-})
 
+  const counter = document.querySelector('.versuche__span');
+  const counterNumber = Number(counter.innerText);
+  console.log(counterNumber)
+  counter.innerText = counterNumber - 1;
+  if(counter.innerText == 0) {
+    mainBlock.style.display='none'
+    overBlock.style.display = 'flex';
+    for(let a in numberQuestion) {
+      overBlock.querySelector('.ovet__answer').innerText = `${numberQuestion[a]}`
+    }
+    // overBlock.querySelector('.ovet__answer');
+
+  } else {
+    mainBlock.style.display='inherit'
+    overBlock.style.display = 'none'
+  }
+})
+fertigButton.addEventListener('click', ()=> {
+  mainBlock.style.display='none'
+  overBlock.style.display = 'flex';
+  for(let a in numberQuestion) {
+    overBlock.querySelector('.ovet__answer').innerText = `${numberQuestion[a]}`
+  }
+})
